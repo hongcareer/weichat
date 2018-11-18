42842d89b7da56454c5738ca5a85853e8f55f366
hongcareer <hongcareer@126.com>
2018-11-18 23:00:03 +0800

    优化事件的模板

--- wei/allNews/tempelate.js
+++ wei/allNews/tempelate.js
@@ -17,6 +17,24 @@ module.exports = function(options){
       <Url><![CDATA[${options.url}]]></Url>
       </item>
       </Articles>`
+  }else if (options.msgType === 'image') {
+    replyMessage += `<Image><MediaId><![CDATA[${options.mediaId}]]></MediaId></Image>`;
+  } else if (options.msgType === 'voice') {
+    replyMessage += `<Voice><MediaId><![CDATA[${options.mediaId}]]></MediaId></Voice>`;
+  } else if (options.msgType === 'video') {
+    replyMessage += `<Video>
+      <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+      <Title><![CDATA[${options.title}]]></Title>
+      <Description><![CDATA[${options.description}]]></Description>
+      </Video>`;
+  } else if (options.msgType === 'music') {
+    replyMessage += `<Music>
+      <Title><![CDATA[${options.title}]]></Title>
+      <Description><![CDATA[${options.description}]]></Description>
+      <MusicUrl><![CDATA[${options.musicUrl}]]></MusicUrl>
+      <HQMusicUrl><![CDATA[${options.hqMusicUrl}]]></HQMusicUrl>
+      <ThumbMediaId><![CDATA[${options.mediaId}]]></ThumbMediaId>
+      </Music>`;
   }
   // else if (options.msgType === 'voice') {
   //   replyMessage += `<Voice><MediaId><![CDATA[${options.mediaId}]]></MediaId></Voice>`;
